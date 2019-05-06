2e6de70ded8a51697fbc1b053cae9512b17a87c8
Aidarous-Jibril <42621649+Aidarous-Jibril@users.noreply.github.com>
2019-05-06 14:46:39 +0200

    Update nav.php

--- include/nav.php
+++ include/nav.php
@@ -1,26 +1,4 @@
 
- <!-- Html way
-
-     <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="index.php">HOME</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="#about">TEAM</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="#courses">MENU</a>
-          </li> 
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="#contact">CONTACT</a>
-          </li>
-
-
-    </ul>
--->
-
-
-
   <ul class="navbar-nav">
       <?php
             //looping throught nav items
